16bd5e72aa43dd63e3fab2688fa2b08a372be548
pyounani <yndbsk9372@gmail.com>
2024-10-21 14:47:47 +0900

    [Feat] DataSource 적용

---
+++ src/test/java/pyounani/jdbc/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,59 @@
+package pyounani.jdbc.repository;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pyounani.jdbc.domain.Member;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static pyounani.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+class MemberRepositoryV1Test {
+
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        repository = new MemberRepositoryV1(dataSource);
+    }
+
+    @Test
+    void crud() throws SQLException, InterruptedException {
+
+        // save
+        Member member = new Member("memberV1", 10000);
+        repository.save(member);
+
+        // findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember = {}", findMember);
+
+        assertThat(findMember).isEqualTo(member);
+        assertThat(findMember).isNotSameAs(member); // 인스턴스는 다르다.
+
+        // update: money 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        // delete
+        repository.delete(member.getMemberId());
+        assertThrows(NoSuchElementException.class, () -> {
+            repository.findById(member.getMemberId());
+        });
+
+        Thread.sleep(1000);
+    }
+}
